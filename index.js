9883c2324a61947a77004da5f1084a87a57c38fd
alfa <muhamadalfarizy20@gmail.com>
2023-06-08 17:02:04 +0700

    inisialisasi2

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const express = require('express')
-
 const app = express()
 const port = 3333
 
 app.get('/', (req, res) => {
-    res.send("contoh hal 1")
+    res.send("contoh hal 1 ")
 }) 
 
-app.listen(port)
+app.get('/about', (req, res) =>{
+    res.send('about')
+})
+
+app.get('/user/:id', (req, res) => {
+    const id = req.params.id;
+    res.send(`user id ${id}`);
+})
+
+app.listen(port, () => console.log(`server running on port ${port}`))
